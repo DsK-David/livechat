779f9ec63079f27379e2b4c8bdff336adb0157f3
David silva <89320886+DsK-David@users.noreply.github.com>
2022-10-14 23:19:02 -0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,34 @@
+// add express and socket
+
+
+const express = require("express")
+const path = require("path");
+const { Socket } = require("socket.io");
+
+
+const app = express();
+
+const server = require("http").createServer(app);
+
+const io = require("socket.io")(server);
+
+// add views and html 
+
+app.use("/", (req,res) => {
+
+    res.render("index.html");
+}); 
+// conect to server 
+let messages = [];
+io.on("connection",Socket =>  {
+    console.log(`socket conectado`);
+
+    Socket.emit('previousMessages', messages);
+    Socket.on("sendMessage" , data => {
+      messages.push(data);
+      Socket.broadcast.emit("receivedMessage",data)
+     
+    })
+});
+// listen port 3000
+server.listen("3000")
